798820bf04622203a353343a71b4850373cfa7b9
Greald <32031553+greald@users.noreply.github.com>
2026-06-10 04:15:19 +0200

    kings as an example
    test files added

---
+++ kings.ex.js
@@ -0,0 +1,165 @@
+  royals = [
+    {
+      "country": "Netherlands",
+      "dynasty": "House of Orange-Nassau",
+      "monarch": {
+        "name": "Willem-Alexander",
+        "birth_date": "1967-04-27"
+      },
+      "predecessor": {
+        "name": "Beatrix",
+        "birth_date": "1938-01-31"
+      },
+      "heir": {
+        "name": "Catharina-Amalia",
+        "birth_date": "2003-12-07"
+      }
+    },
+    {
+      "country": "Belgium",
+      "dynasty": "House of Saxe-Coburg and Gotha",
+      "monarch": {
+        "name": "Philippe",
+        "birth_date": "1960-04-15"
+      },
+      "predecessor": {
+        "name": "Albert II",
+        "birth_date": "1934-06-06"
+      },
+      "heir": {
+        "name": "Elisabeth",
+        "birth_date": "2001-10-25"
+      }
+    },
+    {
+      "country": "United Kingdom",
+      "dynasty": "House of Windsor",
+      "monarch": {
+        "name": "Charles III",
+        "birth_date": "1948-11-14"
+      },
+      "predecessor": {
+        "name": "Elizabeth II",
+        "birth_date": "1926-04-21"
+      },
+      "heir": {
+        "name": "William",
+        "birth_date": "1982-06-21"
+      }
+    },
+    {
+      "country": "Spain",
+      "dynasty": "House of Bourbon",
+      "monarch": {
+        "name": "Felipe VI",
+        "birth_date": "1968-01-30"
+      },
+      "predecessor": {
+        "name": "Juan Carlos I",
+        "birth_date": "1938-01-05"
+      },
+      "heir": {
+        "name": "Leonor",
+        "birth_date": "2005-10-31"
+      }
+    },
+    {
+      "country": "Sweden",
+      "dynasty": "House of Bernadotte",
+      "monarch": {
+        "name": "Carl XVI Gustaf",
+        "birth_date": "1946-04-30"
+      },
+      "predecessor": {
+        "name": "Gustaf VI Adolf",
+        "birth_date": "1882-11-11"
+      },
+      "heir": {
+        "name": "Victoria",
+        "birth_date": "1977-07-14"
+      }
+    },
+    {
+      "country": "Norway",
+      "dynasty": "House of Glücksburg",
+      "monarch": {
+        "name": "Harald V",
+        "birth_date": "1937-02-21"
+      },
+      "predecessor": {
+        "name": "Olav V",
+        "birth_date": "1903-07-02"
+      },
+      "heir": {
+        "name": "Haakon",
+        "birth_date": "1973-07-20"
+      }
+    },
+    {
+      "country": "Denmark",
+      "dynasty": "House of Glücksburg",
+      "monarch": {
+        "name": "Frederik X",
+        "birth_date": "1968-05-26"
+      },
+      "predecessor": {
+        "name": "Margrethe II",
+        "birth_date": "1940-04-16"
+      },
+      "heir": {
+        "name": "Christian",
+        "birth_date": "2005-10-15"
+      }
+    },
+    {
+      "country": "Monaco",
+      "dynasty": "House of Grimaldi",
+      "monarch": {
+        "name": "Albert II",
+        "birth_date": "1958-03-14"
+      },
+      "predecessor": {
+        "name": "Rainier III",
+        "birth_date": "1923-05-31"
+      },
+      "heir": {
+        "name": "Jacques",
+        "birth_date": "2014-12-10"
+      }
+    },
+    {
+      "country": "Luxembourg",
+      "dynasty": "House of Nassau-Weilburg",
+      "monarch": {
+        "name": "Henri",
+        "birth_date": "1955-04-16"
+      },
+      "predecessor": {
+        "name": "Jean",
+        "birth_date": "1921-01-05"
+      },
+      "heir": {
+        "name": "Guillaume",
+        "birth_date": "1981-11-11"
+      }
+    },
+    {
+      "country": "Liechtenstein",
+      "dynasty": "House of Liechtenstein",
+      "monarch": {
+        "name": "Hans-Adam II",
+        "birth_date": "1945-02-14"
+      },
+      "predecessor": {
+        "name": "Franz Joseph II",
+        "birth_date": "1906-08-16"
+      },
+      "heir": {
+        "name": "Alois",
+        "birth_date": "1968-06-11"
+      }
+    }
+  ]
+  
+  wa = royals[0];
+
